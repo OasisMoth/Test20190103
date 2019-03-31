7323b9079f81b5be7574cb8de9a626e1f8b0be0d
OasisMoth <Tanupon.DeepSleep@gmail.com>
2019-03-31 21:40:10 +0900

    Add Proto ConfigWindow

---
+++ FileAssortment/ViewModel/ConfigWindowDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAssortment.ViewModel
+{
+    public class ConfigWindowDataContext : DataContextBase
+    {
+        public string FolderCreateValue
+        {
+            get { return this._FolderCreateValue; }
+            set { if (_FolderCreateValue != value) this.SetAndNotify(ref _FolderCreateValue, value); }
+        }
+        private string _FolderCreateValue;
+
+    }
+}
---
+++ FileAssortment/WindowService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileAssortment
+{
+    public interface IWindowService
+    {
+        void ShowDialog();
+    }
+
+    public class WindowService
+    {
+        public WindowService(Window owner, DataContextBase dataContext)
+        {
+            this._DataContext = dataContext;
+            this._Owner = owner;
+        }
+
+        protected Window _Owner;
+
+        protected DataContextBase _DataContext;
+    }
+
+    public class ConfigWindowService : WindowService, IWindowService
+    {
+        public ConfigWindowService(Window owner, DataContextBase dataContext) : base(owner, dataContext) { }
+
+        public  void ShowDialog()
+        {
+            var configWindow = new ConfigWindow()
+            {
+                DataContext = this._DataContext,
+                Owner = this._Owner,
+            };
+
+            configWindow.ShowDialog();
+        }
+    }
+}
